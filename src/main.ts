647de7e1e5ed6ad13d72836074bacfd688354953
YasminGd <thegodha1@gmail.com>
2023-03-06 15:57:10 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,38 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  let corsOptions;
+  if (process.env.NODE_ENV === 'production') {
+    // Express serve static files on production environment
+    corsOptions = {
+      origin: [
+        'https://theproductloundge.com',
+        'https://myseat.theproductlounge.com',
+      ],
+      credentials: true,
+    };
+
+    // app.use(express.static(path.resolve(__dirname, 'public')))
+  } else {
+    // Configuring CORS
+    corsOptions = {
+      // Make sure origin contains the url your frontend is running on
+      origin: [
+        'http://127.0.0.1:8080',
+        'http://localhost:8080',
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+      ],
+      credentials: true,
+    };
+  }
+  app.enableCors(corsOptions);
+  await app.listen(3030);
 }
 bootstrap();
