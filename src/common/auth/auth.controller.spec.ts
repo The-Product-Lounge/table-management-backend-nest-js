648a6bbb8b6068af966b9db1e6a8838217f60c98
AlmogH <almoghagbi96@gmail.com>
2023-06-09 12:48:03 +0300

    add decorators unit tests

--- src/common/auth/auth.controller.spec.ts
+++ src/common/auth/auth.controller.spec.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { AppConfigService } from '../app-config/app-config.service';
 import { UnauthorizedException } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 const mockJwtService = {
   sign: () => 'mock-token',
@@ -28,11 +29,48 @@ describe('AuthController', () => {
     authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(authController).toBeDefined();
+  describe('@Controller', () => {
+    it('should be defined', () => {
+      expect(authController).toBeDefined();
+    });
+
+    it('should have the correct path', () => {
+      const path = Reflect.getMetadata('path', AuthController);
+      expect(path).toBe('auth');
+    });
   });
 
   describe('login', () => {
+    describe('@UseGuards', () => {
+      it('should use the LocalAuthGuard', () => {
+        const guards = Reflect.getMetadata(
+          '__guards__',
+          AuthController.prototype.login,
+        );
+        expect(guards).toEqual(expect.arrayContaining([LocalAuthGuard]));
+      });
+    });
+
+    describe('@Post', () => {
+      it('should have the correct path', () => {
+        const path = Reflect.getMetadata(
+          'path',
+          AuthController.prototype.login,
+        );
+        expect(path).toBe('login');
+      });
+    });
+
+    describe('@HttpCode', () => {
+      it('should have the correct status code', () => {
+        const statusCode = Reflect.getMetadata(
+          '__httpCode__',
+          AuthController.prototype.login,
+        );
+        expect(statusCode).toBe(200);
+      });
+    });
+
     it('should return a JWT token', async () => {
       const user = { username: 'user', userId: 1 };
       const token = 'jwt-token';
