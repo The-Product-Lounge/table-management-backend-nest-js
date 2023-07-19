27dbb455c568606022fd53e2468a30654b57a124
AlmogH <almoghagbi96@gmail.com>
2023-07-19 14:40:46 +0300

    creating new resource for events using nest generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TableModule } from './table/table.module';
 import { CommonModule } from './common/common.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [TableModule, CommonModule],
+  imports: [TableModule, CommonModule, EventModule],
 })
 export class AppModule {}
